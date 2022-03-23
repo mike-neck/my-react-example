7b73ea240947c75c198699973f8a87921f864b56
mike_neck <jkrt3333@gmail.com>
2022-03-23 12:16:25 +0900

    create project

---
+++ src/fizzbuzz-toValue.test.ts
@@ -0,0 +1,13 @@
+import {toValue, Value} from "./fizzbuzz";
+
+test("toValue 1 becomes {original:1,text:undefined}", () => {
+    expect<Value>(toValue(1)).toStrictEqual({ original: 1, text: undefined});
+});
+
+test("toValue 2 becomes {original:2,text:undefined}", () => {
+    expect<Value>(toValue(2)).toStrictEqual({ original: 1, text: undefined});
+});
+
+test("toValue 3 becomes {original:3,text:undefined}", () => {
+    expect<Value>(toValue(3)).toStrictEqual({ original: 3, text: "Fizz"});
+});
